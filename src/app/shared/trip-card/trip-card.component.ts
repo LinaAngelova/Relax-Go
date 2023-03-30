656f37909eae88ccd1e4a19cfdd98e8ccf630105
LinaAngelova <lina.anguelova@gmail.com>
2023-03-30 21:04:47 +0200

    fix card

--- src/app/shared/trip-card/trip-card.component.ts
+++ src/app/shared/trip-card/trip-card.component.ts
@@ -8,22 +8,28 @@ export class TripCardComponent implements OnInit{
   list : Viaggio[] = [
     {
       _id: 1,
-      title: 'Vacanze al mare',
-      description: 'Se ami il Mediterraneo, troverai le migliori vacanze al mare in Italia. Sogni una vacanza in Sardegna? Scopri i pacchetti volo + hotel di lastminute.com. Oppure scegli una vacanza in Spagna, con le classiche Baleari e Canarie, o le famose Costa Brava e Costa del Sol. Sogni le vacanze in Grecia? Scopri le offerte per le isole greche: Mykonos, Santorini, Corfù, Creta, Kos e Rodi. Qualcosa di più.. esotico? Prenota una vacanza alle Maldive o alle Mauritius, Cuba o Santo Domingo, o Kenya e Zanzibar per un viaggio da sogno, senza dimenticare la meta più celebre per chi ama lo snorkeling: il Mar Rosso.',
-      urlImage : "carousel-1.webp"
+      title: 'Capo verde',
+      description: 'Questo straordinario paese si trova al largo della costa settentrionale dell’Africa e comprende una decina di isole; è famoso anche per l’incredibile cultura afro-portoghese, figlia di una straordinaria contaminazione fra più popolazioni. Oggi è molto amata per le splendide spiagge, la cultura antichissima e la musica morna, che ha influenzato molti artisti moderni. Paesaggi straordinari, onde che si susseguono placide su spiagge spettacolari e tanto verde promettono di regalarvi una vacanza unica, che ricorderete per sempre.',
+      urlImage : "capoVerde.jpg"
     },
     {
       _id: 2,
-      title: 'Vacanze in Europa',
-      description: 'E se ami le città, prenota un volo+hotel e visita Barcellona, Londra, Amsterdam o Lisbona: la vacanza è anche in città! Che tu voglia viaggiare in coppia, da solo, con la famiglia o in gruppo, le vacanze ti stanno aspettando.',
-      urlImage : "carousel-2.webp"
+      title: 'Malta',
+      description: 'ituata in mezzo al Mediterraneo e da sempre crocevia di scambi commerciali, Malta è sempre stata una meta turistica sottovalutata dai viaggiatori a livello mondiale. Motivo? Probabilmente scarse campagne turistiche in passato e la vicinanza di mete attrattive la Sicilia, la Sardegna e la Corsica. Qualcuno di voi starà dicendo: “eppure io ci sono stato quando ero più giovane, per un corso d’Inglese!”. Vero. Malta è tutt’ora una delle mete predilette per chi vuole imparare l’inglese in quanto è uno stato bilingue facente parte in passato del regno britannico e per di più i corsi d’inglese a Malta risultano più economici di quelli nel Regno Unito.',
+      urlImage : "Malta.jpg"
     },
     {
       _id: 3,
       title: 'Parigi',
-      description: 'E se ami le città, prenota un volo+hotel e visita Barcellona, Londra, Amsterdam o Lisbona: la vacanza è anche in città! Che tu voglia viaggiare in coppia, da solo, con la famiglia o in gruppo, le vacanze ti stanno aspettando.',
+      description: 'Parigi è rinomata come meta romantica per eccellenza, ma i visitatori rimangono estasiati dalla città in sé: i grandiosi edifici in pietra e ferro battuto, i marciapiedi costellati di caffetterie accoglienti e le rive serpeggianti della Senna sono davvero mozzafiato. La scena gastronomica offre un elenco infinito di pietanze francesi imperdibili come il coq au vin, piatto ricco e abbondante, e friabili cornetti dorati I falafel della città, ad esempio, sono straordinari. Lo spirito di Parigi ti invita ad addentrarti nelle strade secondarie, a soffermarti nei musei e a esplorare i labirinti di negozi. A fine giornata, ammira la vista indisturbata della Tour Eiffel che risplende nella notte dagli Champ-de-Mars.',
       urlImage : "parigi.jpg"
-    }
+    },
+    {
+      _id: 4,
+      title: 'Mosca',
+      description: 'Dichiarata dal unesco Patrimonio del Umanità non lascia indifferente alcun visitatore. Nella piazza vi sono diversi luoghi da visitare, simboli della storia e della cultura russa: il Cremlino, il Museo Statale di Storia della Russia, la Cattedrale di San Basilio E IL Mausoleo di Lenin. Estendendosi su due continenti e con 14 paesi vicini, la Russia è una combinazione unica di culture e influenze.',
+      urlImage : "Mosca.jpg"
+    },
   ];
 
   percorso = "../assets/images/";
